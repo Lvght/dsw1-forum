7f17e61fe6293322466565868b3b8d51fc4948da
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2021-09-15 12:23:49 -0300

    Adicionada lógica para visualizar comentários

--- src/main/java/br/ufscar/dsw1/dao/CommentDAO.java
+++ src/main/java/br/ufscar/dsw1/dao/CommentDAO.java
@@ -2,11 +2,13 @@ package br.ufscar.dsw1.dao;
 
 import br.ufscar.dsw1.domain.Comment;
 import br.ufscar.dsw1.domain.Post;
+import br.ufscar.dsw1.domain.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CommentDAO extends GenericDAO {
     /**
@@ -39,9 +41,43 @@ public class CommentDAO extends GenericDAO {
                 return new Comment(
                         resultSet.getLong("id_comentario"),
                         resultSet.getString("conteudo"),
+                        UserDAO.getById(resultSet.getLong("id_autor")),
                         originalPost);
             }
 
+            statement.close();
+            connection.close();
+
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param postId aas
+     * @return Uma lista de [Comment], que PODE SER VAZIA. Nulo, se mal-sucedido.
+     */
+    public static ArrayList<Comment> getAllCommentsFromPost(long postId) {
+        final String query = "SELECT * FROM comentario WHERE id_postagem = ?;";
+        ArrayList<Comment> result = new ArrayList<>();
+
+        try {
+            Connection connection = CommentDAO.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            statement.setLong(1, postId);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next())
+                result.add(new Comment(resultSet.getLong("id_comentario"),
+                        resultSet.getString("conteudo"),
+                        UserDAO.getById(resultSet.getLong("id_autor")),
+                        null));
+
+            return result;
 
         } catch (SQLException exception) {
             exception.printStackTrace();
--- src/main/java/br/ufscar/dsw1/domain/Comment.java
+++ src/main/java/br/ufscar/dsw1/domain/Comment.java
@@ -2,11 +2,13 @@ package br.ufscar.dsw1.domain;
 
 public class Comment {
     private long id;
+    private User author;
     private Post originalPost;
     private String content;
 
-    public Comment(long id, String content, Post originalPost) {
+    public Comment(long id, String content, User author, Post originalPost) {
         this.originalPost = originalPost;
+        this.author = author;
         this.content = content;
         this.id = id;
     }
@@ -25,5 +27,13 @@ public class Comment {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
