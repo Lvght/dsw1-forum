de7dd9fef61fc56c4dd69b621e3619c445909c44
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2021-09-22 16:41:18 -0300

    Envio de emails (gmail) funcionando

---
+++ src/main/java/br/ufscar/dsw1/controller/EmailProviderTest.java
@@ -0,0 +1,34 @@
+package br.ufscar.dsw1.controller;
+
+import br.ufscar.dsw1.utils.EmailService;
+
+import javax.mail.internet.InternetAddress;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "EmailProviderTest", value = "/mailtest")
+public class EmailProviderTest extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        EmailService service = new EmailService();
+
+        InternetAddress from = new InternetAddress("caixa@gov.br", "Caixa Econômica Federal");
+        InternetAddress to = new InternetAddress("viniciusluz@estudante.ufscar.br", "Vinicius");
+
+        String subject1 = "bom dia favor quitar cartão contato: 11 99123-456";
+        String subject2 = "Exemplo Subject com Anexo (Mailtrap/Java)";
+
+        String body1 = "Exemplo mensagem (Mailtrap/Java)";
+        String body2 = "Exemplo mensagem com Anexo (Mailtrap/Java)";
+
+        // Envio sem anexo
+        service.send(from, to, subject1, body1);
+
+        // Envio com anexo
+//        service.send(from, to, subject2, body2);
+    }
+}
---
+++ src/main/java/br/ufscar/dsw1/utils/EmailService.java
@@ -0,0 +1,77 @@
+package br.ufscar.dsw1.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class EmailService {
+
+    public void send(InternetAddress from, InternetAddress to, String subject, String body, File file) {
+
+        try {
+            Properties properties = new Properties();
+
+            final String username = System.getenv("GMAIL_ADDRESS");
+            final String password = System.getenv("GMAIL_APP_PASSWORD");
+            final String host = System.getenv("EMAIL_PROVIDER_HOST");
+            final String port = System.getenv("EMAIL_PROVIDER_PORT");
+
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.ssl.enable", "true");
+            properties.put("mail.smtp.starttls.required", "true");
+            properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+            properties.put("mail.smtp.host", host);
+            properties.put("mail.smtp.port", port);
+
+            Session session = Session.getInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+
+            Message message = new MimeMessage(session);
+            message.setFrom(from);
+            message.setRecipient(Message.RecipientType.TO, to);
+            message.setSubject(subject);
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(body, "text/plain");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            if (file != null) {
+                MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+                attachmentBodyPart.attachFile(file);
+                multipart.addBodyPart(attachmentBodyPart);
+            }
+
+            message.setContent(multipart);
+            Transport.send(message);
+
+            System.out.println("Mensagem enviada com sucesso!");
+
+        } catch (Exception e) {
+            System.out.println("Mensagem não enviada!");
+            e.printStackTrace();
+        }
+    }
+
+    public void send(InternetAddress from, InternetAddress to, String subject, String body) {
+        send(from, to, subject, body, null);
+    }
+}
